a6ac3d48f855cd14484368e73b342b79f267f796
andreshernandez941laptop <andres.hernandez941@gmail.com>
2026-05-25 21:53:09 -0500

    actualizacion con complemento y reverse comeplement

--- DNAtoolkit.py
+++ DNAtoolkit.py
@@ -1,21 +1,26 @@
 import collections
-
-Nucleotides = ["A", "C", "G", "T"]
-dict.DNANucleotides = {"A", "C", "G", "T"}
-dict.RNANucleotides = {"A", "C", "G", "U"}
+from Sequences import *
 
 def validateSeq(dna_seq):
+    """Validates the DNA String"""
     tempseq = dna_seq.upper()
     for nuc in tempseq:
         if nuc not in Nucleotides:
             return False
-    return tempseq + " is a valid DNA sequence";
+    return tempseq;
 
 def countNucFrequency(dna_seq):
+    """Counts Nucleotide frequency"""
     tmpFreqDict = {"A": 0, "C": 0, "G": 0, "T": 0}
     for nuc in dna_seq:
         tmpFreqDict[nuc] += 1
     return tmpFreqDict
 
+def transcription(dna_seq):
+    """Changes Bases to RNA bases"""
+    return dna_seq.replace("T", "U")
+
+def reverse_complement(dna_seq):
+    return ''.join([DNAcomplements[nuc] for nuc in dna_seq])[::-1]
 
 
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from DNAtoolkit import *
+from Utilities import *
 
-print(validateSeq("ACGTAGCTAGCTAGCTAGCTAGCTAGC"))
-print(countNucFrequency("ACCCGGGTCTCTGAGGAGGAGCTCG"))
-print(dict)
+DNAStr = validateSeq("GGTGGTGTGCGGAGCGAGTGCGAGAGTC")
+
+print(f'\nSequence: {colored(DNAStr)}\n')
+print(f"[1] + Sequence lenght: {len(DNAStr)}\n")
+print(colored(f'[2] + Nucleotide frequency: {countNucFrequency(DNAStr)}\n'))
+print(f'[3] + DNA/RNA Transcription: {colored(transcription(DNAStr))}\n')
+print(f"[4] + DNA String + Reverse Complement: \n5' {colored(DNAStr)} 3'")
+print(f"   {''.join(['|' for c in range(len(DNAStr))])}")
+print(colored(f"3' {(reverse_complement(DNAStr))[::-1]}5' [Complement]\n"))
+print(f"5' {colored(reverse_complement(DNAStr))} 3' [Reverse complement]\n")
